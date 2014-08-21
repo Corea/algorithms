fb089660d9fb5255a66b671eda59d9bc5645de28
Hyungsuk Yoon <youhanmir@gmail.com>
2014-08-21 17:41:38 +0900

    update aho corasick

---
+++ string/aho_corasick.cpp
@@ -0,0 +1,55 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+typedef int aho_val_t;
+
+struct AhoCorasick {
+    int K; // the number of character
+    vector<vector<int> > trie;
+    vector<int> fail, value;
+    
+    int convert(char c) { // implement this function
+        return c - 'a';
+    }
+
+    AhoCorasick(const vector<pair<string, aho_val_t> >& data, int k) : 
+            K(k), trie(1, vector<int>(k, 0)), value(1, 0) { 
+        for(auto &s : data) {
+            int now = 0;
+            for(auto &c : s.first) {
+                int &next = trie[now][convert(c)];
+                if(!next) {
+                    next = trie.size();
+                    trie.push_back(vector<int>(K, 0));
+                    value.push_back(0);
+                }
+                now = next;
+            }
+            value[now] = s.second;
+        }
+
+        fail.resize(trie.size(), 0);
+
+        queue<int> Q; Q.push(0);
+        while(!Q.empty()) {
+            int x = Q.front(); Q.pop();
+            for(int i = 0; i < K; i ++) {
+                if(trie[x][i]){
+                    int now = fail[x];
+                    int next = trie[x][i];
+                    if(x) {
+                        while(now && !trie[now][i]) {
+                            now = fail[now];
+                        }
+                        now = trie[now][i];
+                    }
+                    fail[next] = now;
+                    Q.push(next);
+                }
+            }
+        }
+    }
+};
